d81b6b607f1898a7d6c46a28f6c958304e92e086
ix4 <xion@protonmail.com>
2019-11-15 03:40:47 -0500

    new pdf

--- web/viewer.js
+++ web/viewer.js
@@ -4119,7 +4119,7 @@ var defaultOptions = {
     kind: OptionKind.VIEWER + OptionKind.PREFERENCE
   },
   defaultUrl: {
-    value: 'compressed.tracemonkey-pldi-09.pdf',
+    value: 'b.pdf',
     kind: OptionKind.VIEWER
   },
   defaultZoomValue: {
